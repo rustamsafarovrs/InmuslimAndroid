07c5486fa868a24fc5e2d461bcbd7f0587aa989f
rustamsafarovrs <rs.programmer@hotmail.com>
2023-08-20 22:47:51 +0500

    [fix] ColorSchema

--- app/src/main/java/tj/rsdevteam/inmuslim/ui/common/Components.kt
+++ app/src/main/java/tj/rsdevteam/inmuslim/ui/common/Components.kt
@@ -6,13 +6,13 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import tj.rsdevteam.inmuslim.ui.theme.InmuslimTypography
-import tj.rsdevteam.inmuslim.ui.theme.primaryColor
 
 /**
  * Created by Rustam Safarov on 8/13/23.
@@ -23,7 +23,7 @@ import tj.rsdevteam.inmuslim.ui.theme.primaryColor
 fun PrimaryButton(text: String, onClick: () -> Unit) {
     androidx.compose.material3.Button(
         onClick = onClick,
-        colors = ButtonDefaults.buttonColors(containerColor = primaryColor),
+        colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.primary),
         elevation = ButtonDefaults.buttonElevation(defaultElevation = 0.dp),
         modifier = Modifier
             .fillMaxWidth()
